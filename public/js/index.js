6a7f5af37a4526632db239848c68dfddc1c9bb91
Kenneth <scriptoss@gmail.com>
2019-01-04 11:33:07 +1300

    Scoll to bottom is user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+const scrollToBottom = () =>{
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  };
+};
+
 socket.on('connect', () => {
   console.log('Connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', (message) =>  {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', (message) => {
@@ -30,6 +47,7 @@ socket.on('newLocationMessage', (message) => {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 
